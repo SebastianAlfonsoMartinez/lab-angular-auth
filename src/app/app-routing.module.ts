843787c189e1d4329ff7ff1814ff550215e118b8
Ferney Peña Rivillas <ferney.penarivillas@teck.com>
2022-09-20 21:31:18 -0500

    Se organiza la página de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
   {
     path: 'main',
     component: MainComponent,
-    ...canActivate(() => redirectUnauthorizedTo(['/register']))
+    ...canActivate(() => redirectUnauthorizedTo(['/login']))
   },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent }
